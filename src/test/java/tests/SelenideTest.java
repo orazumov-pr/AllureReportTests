cdd423246e086aafdc1f307e15bbbafa284627ef
Oleg Razumov <orazumoff@gmail.com>
2026-03-22 11:24:51 +0300

    change import

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -1,13 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.BaseElementsCollection;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
